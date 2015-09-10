a5e24ff766af8b0153855692a91cd87522aae43c
bsdshell <monosurface@gmail.com>
2015-09-10 16:33:13 -0700

    test point if it is inside the rectangle

---
+++ OneTestPointInsideRectangle/SimpleApp/ViewController.h
@@ -0,0 +1,26 @@
+//
+//  ViewController.h
+//  tut1
+//
+//  Created by aa aa on 12-06-16.
+//  Copyright (c) 2012 asdfk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    CAShapeLayer* _shapeLayer;
+    CAShapeLayer* _rectLayer;
+}
+
+@property(nonatomic, retain)CAShapeLayer* shaperLayer;
+@property(nonatomic, retain)CAShapeLayer* rectLayer;
+@end
+
+@interface MyButton: UIButton
+
+-(id) initWithFrame:(CGRect)frame;
+-(void) clickMe:(id) sender;
+
+@end
