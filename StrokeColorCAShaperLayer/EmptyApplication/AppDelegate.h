c8a7959979c721753afbed4a89cd95bb7d7d1c8f
bsdshell <monosurface@gmail.com>
2016-02-26 16:27:34 -0800

    add StrokeColor CAShaperLayer

---
+++ StrokeColorCAShaperLayer/EmptyApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EmptyApplication
+//
+//  Created by cat on 5/6/14.
+//  Copyright (c) 2014 searchBarDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
