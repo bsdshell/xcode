3b45975a2edd92d626f5a1a85542f498a7a5c249
bsdshell <monosurface@gmail.com>
2016-03-03 13:39:10 -0800

    add CAShapeLayer Gradient Rectangle

--- OneGradientLayer/EmptyApplication/AppDelegate.h
+++ OneGradientLayer/EmptyApplication/AppDelegate.h
@@ -1,13 +1,4 @@
-//
-//  AppDelegate.h
-//  EmptyApplication
-//
-//  Created by cat on 5/6/14.
-//  Copyright (c) 2014 searchBarDemo. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
