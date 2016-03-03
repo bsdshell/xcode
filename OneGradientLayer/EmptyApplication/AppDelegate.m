53f813778b3dc97c931e43692730f326faeecd16
bsdshell <monosurface@gmail.com>
2016-03-03 13:40:11 -0800

    add CAShapeLayer Gradient Rectangle

--- OneGradientLayer/EmptyApplication/AppDelegate.m
+++ OneGradientLayer/EmptyApplication/AppDelegate.m
@@ -1,12 +1,9 @@
 #import "AppDelegate.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    
     self.window.backgroundColor = [UIColor whiteColor];
 
     // searchkey: gradient CASHapeLayer gradient   close path closepath
