8eed2863625faa15823e16902062923efa4f92e6
bsdshell <monosurface@gmail.com>
2016-03-04 20:10:30 -0800

    add more

--- OneCAShapeLayerTransform/EmptyApplication/AppDelegate.h
+++ OneCAShapeLayerTransform/EmptyApplication/AppDelegate.h
@@ -7,16 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MyViewController.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>{
     NSInteger _numClick;
     CAShapeLayer* _shapeLayer;
     CAShapeLayer* _curveLayer;
     UIBezierPath* _curve;
+    CGPoint _mycenter;
     double _semiWidth;
     double _semiHeight;
     CGPoint _upLeft;
     NSTimer* _tickFinish;
+    MyViewController* _myView;
+    CATransform3D _transform;
+    bool _axisX;
+    bool _axisY;
+    bool _axisZ;
+    NSInteger _rotX;
+    NSInteger _rotY;
+    NSInteger _rotZ;
+    
+    
 }
 
 @property (strong, nonatomic) UIWindow *window;
--- OneCAShapeLayerTransform/EmptyApplication/AppDelegate.m
+++ OneCAShapeLayerTransform/EmptyApplication/AppDelegate.m
@@ -7,107 +7,163 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-    _numClick = 0;
-
-    _semiWidth = 100;
-    _semiHeight = 100;
-    _upLeft = CGPointMake(100, 100);
-
+    _myView = [[MyViewController alloc]init];
+    
+    CGSize size          = [UIScreen mainScreen].bounds.size;
+    //----------------------------------------------------------
+    _axisX = false;
+    _axisY = false;
+    _axisZ = false;
+    _rotX = 0;
+    _rotY = 0;
+    _rotZ = 0;
+    
     _shapeLayer = [CAShapeLayer layer];
-    _curveLayer = [CAShapeLayer layer];
+    CGFloat Width = 200.0f;
+    CGFloat Height = 200.0f;
+    _transform = CATransform3DIdentity;
 
-    UIBezierPath* path = [UIBezierPath bezierPathWithRect:CGRectMake(100, 100, 200, 200)];
-    UIBezierPath* path1 = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(100, 100, 200, 200)];
+    _mycenter = CGPointMake(size.width/2 - Width/2, size.height/2 - Height/2);
+    //UIBezierPath* path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(_mycenter.x, _mycenter.y, Width, Height)];
+    UIBezierPath* path = [UIBezierPath bezierPathWithRect:CGRectMake(_mycenter.x, _mycenter.y, Width, Height)];
 
-//    UIBezierPath* path = [UIBezierPath bezierPathWithRect:CGRectMake(-100, -100, 200, 200)];
-//    UIBezierPath* path1 = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(-100, -100, 200, 200)];
     
-    //UIBezierPath* path = [UIBezierPath bezierPath];
+    [_shapeLayer setPath:[path CGPath]];
 
-//    [path moveToPoint:CGPointMake(_upLeft.x - _semiWidth, _upLeft.y - _semiHeight)];
-//    
-//    [path addLineToPoint:CGPointMake(_upLeft.x + _semiWidth, _upLeft.y - _semiHeight)];
-//    [path addLineToPoint:CGPointMake(_upLeft.x + _semiWidth, _upLeft.y + _semiHeight)];
-//    [path addLineToPoint:CGPointMake(_upLeft.x - _semiWidth, _upLeft.y + _semiHeight)];
-//    [path closePath];
-    _shapeLayer.path = [path CGPath];
-    _curveLayer.path = [path1 CGPath];
     
+    _shapeLayer.lineWidth = 10.0f;
     _shapeLayer.strokeColor = [[UIColor brownColor] CGColor];
-    _shapeLayer.fillColor = [[UIColor clearColor] CGColor];
-    _shapeLayer.lineWidth = 10;
-
+    [_shapeLayer setFillColor:[[UIColor clearColor] CGColor]];
+//    _shapeLayer.anchorPoint = CGPointMake(0.0f, 0.0f);
     
-    _curveLayer.strokeColor = [[UIColor greenColor] CGColor];
-    _curveLayer.fillColor = [[UIColor clearColor] CGColor];
-    _curveLayer.lineWidth = 10;
-
-
     CGPoint anchorPoint = self.window.layer.anchorPoint;
     NSLog(@"init_anchor_x=[%f] init_anchor_y=[%f] ", anchorPoint.x, anchorPoint.y);
     
-    [self.window.layer addSublayer:_shapeLayer];
-    [self.window.layer addSublayer:_curveLayer];
-    //----------------------------------------------------------
-    UIButton* mybut = [UIButton buttonWithType:UIButtonTypeSystem];
-    CGRect frame = CGRectMake(100, 450, 140, 50);
-    mybut.frame = frame;
-    [mybut addTarget:self action:@selector(startClick1:) forControlEvents:UIControlEventTouchUpInside];
-    [mybut setTitle:@"Rotate" forState:(UIControlState) UIControlStateNormal];
-    [mybut setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [mybut.titleLabel setFont:[UIFont boldSystemFontOfSize:14]];
-    mybut.backgroundColor = [UIColor brownColor];
-    [self.window addSubview:mybut];
-
-    [self startFinishGameTimer];
+    [_myView.view.layer addSublayer:_shapeLayer];
+    
+    [self.window addSubview:_myView.view];
+    
+    
+    [self myButtonX];
+    [self myButtonY];
+    [self myButtonZ];
+   [self startFinishGameTimer];
     
     [self.window makeKeyAndVisible];
     return YES;
 }
 
--(void)startClick1:(id)sender{
-    //NSLog(@"Click0 me");
-    if(_numClick < 100)
-        _numClick++;
-    else
-        _numClick = 1;
-    
-//    CATransform3D translate = CATransform3DMakeTranslation(-200.0, -200.0f, 0.0);
-//    CATransform3D rotate = CATransform3DMakeRotation(_numClick*(M_1_PI/30.0f), 0.0, 0.0, 1.0);
-//    CATransform3D translateback = CATransform3DMakeTranslation(200.0f, 200.0f, 0.0f);
-//    CATransform3D finalTransform = CATransform3DConcat(CATransform3DConcat(translate, rotate), translateback);
+-(void)myButtonX{
+    UIButton* mybut = [UIButton buttonWithType:UIButtonTypeSystem];
+    CGRect frame = CGRectMake(2, 450, 100, 50);
+    mybut.frame = frame;
+    [mybut addTarget:self action:@selector(clickRotX:) forControlEvents:UIControlEventTouchUpInside];
+    [mybut setTitle:@"Rotate X" forState:(UIControlState) UIControlStateNormal];
+    [mybut setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [mybut.titleLabel setFont:[UIFont boldSystemFontOfSize:14]];
+    mybut.backgroundColor = [UIColor brownColor];
+    [self.window addSubview:mybut];
+}
 
-    CATransform3D translate = CATransform3DMakeTranslation(-200.0, -200.0f, 0.0);
-    CGFloat radians =_numClick*(2*M_PI/100.0f);
-    CATransform3D rotate = CATransform3DMakeRotation(radians, 0.0, 0.0, 1.0);
-    CATransform3D translateback = CATransform3DMakeTranslation(200.0f, 200.0f, 0.0f);
+-(void)myButtonY{
+    UIButton* mybut = [UIButton buttonWithType:UIButtonTypeSystem];
+    CGRect frame = CGRectMake(110, 450, 100, 50);
+    mybut.frame = frame;
+    [mybut addTarget:self action:@selector(clickRotY:) forControlEvents:UIControlEventTouchUpInside];
+    [mybut setTitle:@"Rotate Y" forState:(UIControlState) UIControlStateNormal];
+    [mybut setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [mybut.titleLabel setFont:[UIFont boldSystemFontOfSize:14]];
+    mybut.backgroundColor = [UIColor brownColor];
+    [self.window addSubview:mybut];
+}
+
+-(void)myButtonZ{
+    UIButton* mybut = [UIButton buttonWithType:UIButtonTypeSystem];
+    CGRect frame = CGRectMake(230, 450, 100, 50);
+    mybut.frame = frame;
+    [mybut addTarget:self action:@selector(clickRotZ:) forControlEvents:UIControlEventTouchUpInside];
+    [mybut setTitle:@"Rotate Z" forState:(UIControlState) UIControlStateNormal];
+    [mybut setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [mybut.titleLabel setFont:[UIFont boldSystemFontOfSize:14]];
+    mybut.backgroundColor = [UIColor brownColor];
+    [self.window addSubview:mybut];
+}
+
+-(void)clickRotX:(id)sender{
+    _axisX = true;
+    _axisY = false;
+    _axisZ = false;
+}
+
+-(void)clickRotY:(id)sender{
+    _axisX = false;
+    _axisY = true;
+    _axisZ = false;
+}
+
+-(void)clickRotZ:(id)sender{
+    _axisX = false;
+    _axisY = false;
+    _axisZ = true;
+}
+
+-(void)startClick1:(id)sender{
+     _transform = CATransform3DIdentity;
+     _transform.m34 = -1.0/600.0f;
+    CGFloat radians = 1*(2*M_PI/100.0f);
     
-    CATransform3D finalTransform = CATransform3DConcat(CATransform3DConcat(translate, rotate), translateback);
+    if(_axisX){
+        if(_rotX < 100)
+            _rotX++;
+        else
+            _rotX = 1;
+        
+    }
+    CGFloat rotValue = _rotX*radians;
+
+    _transform = CATransform3DRotate(_transform, rotValue, 1.0, 0.0, 0.0);
+
+
+    if(_axisY){
+        if(_rotY < 100)
+            _rotY++;
+        else
+            _rotY = 1;
+
+    }
+    rotValue = _rotY*radians;
+    _transform = CATransform3DRotate(_transform, rotValue, 0.0, 1.0, 0.0);
     
-//    finalTransform.m34 = -1.0/800.0f;
+    if(_axisZ){
+        if(_rotZ < 100)
+            _rotZ++;
+        else
+            _rotZ = 1;
+        
+    }
+    rotValue = _rotZ*radians;
+    _transform = CATransform3DRotate(_transform, rotValue, 0.0, 0.0, 1.0);
     
-    _shapeLayer.transform = finalTransform;
+    [_myView.view.layer setTransform:_transform];
     
     CGPoint anchorPoint = self.window.layer.anchorPoint;
     CGPoint position = self.window.layer.position;
     NSLog(@"anchor_x=[%f] anchor_y=[%f]  position_x=[%f], position_y=[%f]", anchorPoint.x, anchorPoint.y, position.x, position.y);
-
     
     NSLog(@"numClick=[%ld]", _numClick);
     NSLog(@"radians =[%f]", radians);
     NSLog(@"---------------------------------------------");
-    NSLog(@"[%f] [%f] [%f] [%f]", rotate.m11, rotate.m12, rotate.m13, rotate.m14);
-    NSLog(@"[%f] [%f] [%f] [%f]", rotate.m21, rotate.m22, rotate.m23, rotate.m24);
-    NSLog(@"[%f] [%f] [%f] [%f]", rotate.m31, rotate.m32, rotate.m33, rotate.m34);
-    NSLog(@"[%f] [%f] [%f] [%f]", rotate.m41, rotate.m42, rotate.m43, rotate.m44);
+    NSLog(@"[%f] [%f] [%f] [%f]", _transform.m11, _transform.m12, _transform.m13, _transform.m14);
+    NSLog(@"[%f] [%f] [%f] [%f]", _transform.m21, _transform.m22, _transform.m23, _transform.m24);
+    NSLog(@"[%f] [%f] [%f] [%f]", _transform.m31, _transform.m32, _transform.m33, _transform.m34);
+    NSLog(@"[%f] [%f] [%f] [%f]", _transform.m41, _transform.m42, _transform.m43, _transform.m44);
     NSLog(@"---------------------------------------------");
-    //_curveLayer.transform = rotate;
 }
 
 -(void)startFinishGameTimer{
     if (_tickFinish!= nil)
         [_tickFinish invalidate];
-    _tickFinish = [NSTimer scheduledTimerWithTimeInterval:0.01
+    _tickFinish = [NSTimer scheduledTimerWithTimeInterval:0.05f
                                                    target:self
                                                  selector:@selector(startClick1:)
                                                  userInfo:nil
