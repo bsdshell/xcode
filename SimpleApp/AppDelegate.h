5252880ec32a5f193b4029362de39ee0a062827f
bsdshell <monosurface@gmail.com>
2016-04-02 16:26:08 -0700

    Simplify SimpleApp again

--- SimpleApp/AppDelegate.h
+++ SimpleApp/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  tut1
-//
-//  Created by aa aa on 12-06-16.
-//  Copyright (c) 2012 asdfk. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- SimpleApp/SimpleApp/ViewController.h
+++ SimpleApp/SimpleApp/ViewController.h
@@ -1,20 +1,6 @@
-//
-//  ViewController.h
-//  tut1
-//
-//  Created by aa aa on 12-06-16.
-//  Copyright (c) 2012 asdfk. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
-@end
-
-@interface MyButton: UIButton
-
--(id) initWithFrame:(CGRect)frame;
 -(void) clickMe:(id) sender;
-
 @end
